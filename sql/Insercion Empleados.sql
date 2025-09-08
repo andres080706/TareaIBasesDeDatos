8cb3424eb2f2051f30e5d9b5b2ac6a8e83089e55
AndresHC <142336284+AndresHernandezC@users.noreply.github.com>
2025-09-07 21:11:10 -0600

    Actualizacion pequeña insercion empleado

--- sql/Insercion Empleados.sql
+++ sql/Insercion Empleados.sql
@@ -7,15 +7,17 @@ CREATE OR ALTER PROCEDURE dbo.sp_InsertarEmpleado
 AS
 BEGIN
     SET NOCOUNT ON;
-    SET @Nombre=LTRIM(RTRIM(@Nombre));
 
-    IF (@Nombre IS NULL OR @Nombre='')
+    SET @Nombre = LTRIM(RTRIM(@Nombre));
+
+
+    IF (@Nombre IS NULL OR @Nombre = '')
     BEGIN
         SELECT CAST(1001 AS INT) AS code, N'El nombre es obligatorio.' AS message;
         RETURN;
     END
 
-    IF (@Salario IS NULL OR @Salario<=0)
+    IF (@Salario IS NULL OR @Salario <= 0)
     BEGIN
         SELECT CAST(1002 AS INT) AS code, N'El salario debe ser positivo.' AS message;
         RETURN;
@@ -24,15 +26,16 @@ BEGIN
     IF EXISTS (
         SELECT 1
         FROM dbo.Empleado
-        WHERE UPPER(LTRIM(RTRIM(Nombre)))=UPPER(@Nombre)
+        WHERE UPPER(LTRIM(RTRIM(Nombre))) = UPPER(@Nombre)
     )
     BEGIN
         SELECT CAST(1 AS INT) AS code, N'Nombre de Empleado ya existe.' AS message;
         RETURN;
     END
 
-    INSERT INTO dbo.Empleado (Nombre, Salario)
+    INSERT INTO dbo.Empleado ( Nombre, Salario)
     VALUES (@Nombre, @Salario);
+
     SELECT CAST(0 AS INT) AS code, N'Ok' AS message;
 END
 GO
